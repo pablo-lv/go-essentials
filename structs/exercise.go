a8ee4060ef064bd9b6d07a600c6b31c19d512994
pablo.lucas <pablo.lucv@gmail.com>
2022-05-19 16:43:54 -0500

    Added arrays

---
+++ structs/exercise.go
@@ -0,0 +1,42 @@
+//--Summary:
+//  Create a program to calculate the area and perimeter
+//  of a rectangle.
+//
+//--Requirements:
+//* Create a rectangle structure containing its coordinates
+//* Using functions, calculate the area and perimeter of a rectangle,
+//  - Print the results to the terminal
+//  - The functions must use the rectangle structure as the function parameter
+//* After performing the above requirements, double the size
+//  of the existing rectangle and repeat the calculations
+//  - Print the new results to the terminal
+//
+//--Notes:
+//* The area of a rectangle is length*width
+//* The perimeter of a rectangle is the sum of the lengths of all sides
+
+package main
+
+import "fmt"
+
+type Reactangle struct {
+	length int
+	width  int
+}
+
+func calculate_area(shape Reactangle) int {
+	return shape.length * shape.width
+}
+
+func calculate_perimeter(shape Reactangle) int {
+	return (shape.length + shape.width) * 2
+}
+
+func main() {
+	rectangle := Reactangle{3, 2}
+
+	area := calculate_area(rectangle)
+	fmt.Println(area)
+	perimeter := calculate_perimeter(rectangle)
+	fmt.Println(perimeter)
+}
